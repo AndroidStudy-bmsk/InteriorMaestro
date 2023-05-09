378ffab1f7512e1c4a4f2d9e3843d2719c1b7243
bmsk <vv99911@gmail.com>
2023-05-09 22:40:06 +0900

    Feat: FireStore에 사진 게시

--- app/src/main/java/org/bmsk/interiormaestro/ui/article/WriteArticleFragment.kt
+++ app/src/main/java/org/bmsk/interiormaestro/ui/article/WriteArticleFragment.kt
@@ -1,25 +1,33 @@
 package org.bmsk.interiormaestro.ui.article
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import org.bmsk.interiormaestro.R
 import org.bmsk.interiormaestro.databinding.FragmentWriteArticleBinding
+import java.util.UUID
 
-class WriteArticleFragment: Fragment(R.layout.fragment_write_article) {
+class WriteArticleFragment : Fragment(R.layout.fragment_write_article) {
     private lateinit var binding: FragmentWriteArticleBinding
-    private val pickMedia = registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-        // Callback is invoked after the user selects a media item or closes the
-        // photo picker.
-        if (uri != null) {
-            Log.d("PhotoPicker", "Selected URI: $uri")
-        } else {
-            Log.d("PhotoPicker", "No media selected")
+    private var selectedUri: Uri? = null
+    private val pickMedia =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+
+            if (uri != null) {
+                selectedUri = uri
+                binding.photoImageView.setImageURI(uri)
+            } else {
+
+            }
         }
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,5 +35,43 @@ class WriteArticleFragment: Fragment(R.layout.fragment_write_article) {
 
         pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
 
+        binding.photoImageView.setOnClickListener {
+
+        }
+
+        binding.cancelButton.setOnClickListener {
+            findNavController().navigate(WriteArticleFragmentDirections.actionWriteArticleFragmentToHomeFragmentCancel())
+        }
+
+        binding.submitButton.setOnClickListener {
+            if (selectedUri != null) {
+                val photoUri = selectedUri ?: return@setOnClickListener
+
+                val fileName = "${UUID.randomUUID()}.png"
+                Firebase.storage.reference.child("articles/photo").child(fileName)
+                    .putFile(photoUri)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Firebase.storage.reference.child("articles/photo/$fileName")
+                                .downloadUrl
+                                .addOnSuccessListener {
+                                    Log.e("aa", it.toString())
+                                }.addOnFailureListener {
+
+                                }
+                        } else {
+                            task.exception?.printStackTrace()
+                            // error Handle
+                        }
+                    }
+
+            } else {
+                Snackbar.make(view, getString(R.string.image_not_selected), Snackbar.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.backButton.setOnClickListener {
+            findNavController().navigate(WriteArticleFragmentDirections.actionWriteArticleFragmentToHomeFragmentBack())
+        }
     }
 }
