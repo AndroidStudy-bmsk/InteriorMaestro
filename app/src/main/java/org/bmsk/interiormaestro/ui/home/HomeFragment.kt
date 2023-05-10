79aa59f6a29fc055fd614d0101f98bdf51a099e5
bmsk <vv99911@gmail.com>
2023-05-10 16:53:22 +0900

    Refactor: HomeFragment

--- app/src/main/java/org/bmsk/interiormaestro/ui/home/HomeFragment.kt
+++ app/src/main/java/org/bmsk/interiormaestro/ui/home/HomeFragment.kt
@@ -26,30 +26,25 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
 
-        setUpWriteButton(view)
+        setUpWriteButton()
         setUpBookMarkButton()
         setUpRecyclerView()
         fetchArticlesData()
     }
 
-    private fun setUpWriteButton(view: View) {
+    private fun setUpWriteButton() {
         binding.writeButton.setOnClickListener {
-            if (Firebase.auth.currentUser != null) {
-                val action = HomeFragmentDirections.actionHomeFragmentToWriteArticleFragment()
-                findNavController().navigate(action)
+            if (isUserLoggedIn()) {
+                navigateToWriteArticleFragment()
             } else {
-                Snackbar.make(
-                    view,
-                    getString(R.string.guide_use_after_login),
-                    Snackbar.LENGTH_SHORT
-                ).show()
+                showLoginSnackbar()
             }
         }
     }
 
     private fun setUpBookMarkButton() {
         binding.bookMarkButton.setOnClickListener {
-            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToBookMarkArticleFragment())
+            navigateToBookmarkArticleFragment()
         }
     }
 
@@ -62,34 +57,8 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private fun initHomeArticleAdapter(): HomeArticleAdapter {
         return HomeArticleAdapter(
-            onItemClicked = {
-                findNavController().navigate(
-                    HomeFragmentDirections.actionHomeFragmentToArticleFragment(
-                        articleId = it.articleId.orEmpty()
-                    )
-                )
-            },
-            onBookmarkClicked = { articleId, isBookmark ->
-                val uid = Firebase.auth.currentUser?.uid ?: return@HomeArticleAdapter
-                Firebase.firestore.collection("bookmark").document(uid)
-                    .update(
-                        "articleIds",
-                        if (isBookmark) {
-                            FieldValue.arrayUnion(articleId)
-                        } else {
-                            FieldValue.arrayRemove(articleId)
-                        }
-                    ).addOnFailureListener {
-                        if (it is FirebaseFirestoreException && it.code == FirebaseFirestoreException.Code.NOT_FOUND) {
-                            if (isBookmark) {
-                                Firebase.firestore.collection("bookmark").document(uid)
-                                    .set(
-                                        hashMapOf("articleIds" to listOf(articleId))
-                                    )
-                            }
-                        }
-                    }
-            }
+            onItemClicked = { navigateToArticleFragment(it.articleId.orEmpty()) },
+            onBookmarkClicked = { articleId, isBookmark -> updateBookmark(articleId, isBookmark) }
         )
     }
 
@@ -97,30 +66,77 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         val uid = Firebase.auth.currentUser?.uid ?: return
         Firebase.firestore.collection("bookmark").document(uid)
             .get()
-            .addOnSuccessListener {
-                val bookMarkList = it.get("articleIds") as? List<*>
-
-                Firebase.firestore.collection("articles")
-                    .get()
-                    .addOnSuccessListener { result ->
-                        val list = result
-                            .map { snapshot -> snapshot.toObject<ArticleModel>() }
-                            .map { model ->
-                                ArticleItem(
-                                    articleId = model.articleId.orEmpty(),
-                                    description = model.description.orEmpty(),
-                                    imageUrl = model.imageUrl.orEmpty(),
-                                    isBookmark = bookMarkList?.contains(model.articleId.orEmpty())
-                                        ?: false
-                                )
-                            }
-                        articleAdapter.submitList(list)
-                    }.addOnFailureListener { e ->
-                        e.printStackTrace()
-                    }
+            .addOnSuccessListener { document ->
+                val bookMarkList = document.get("articleIds") as? List<*>
+                fetchArticles(bookMarkList)
             }
             .addOnFailureListener {
-
+                // Handle error
             }
+    }
+
+    private fun fetchArticles(bookMarkList: List<*>?) {
+        Firebase.firestore.collection("articles")
+            .get()
+            .addOnSuccessListener { result ->
+                val list = result.map { snapshot -> snapshot.toObject<ArticleModel>() }
+                    .map { model -> createArticleItem(model, bookMarkList) }
+                articleAdapter.submitList(list)
+            }
+            .addOnFailureListener { e ->
+                e.printStackTrace()
+            }
+    }
+
+    private fun createArticleItem(model: ArticleModel, bookMarkList: List<*>?): ArticleItem {
+        return ArticleItem(
+            articleId = model.articleId.orEmpty(),
+            description = model.description.orEmpty(),
+            imageUrl = model.imageUrl.orEmpty(),
+            isBookmark = bookMarkList?.contains(model.articleId.orEmpty()) ?: false
+        )
+    }
+
+    private fun updateBookmark(articleId: String, isBookmark: Boolean) {
+        val uid = Firebase.auth.currentUser?.uid ?: return
+        val document = Firebase.firestore.collection("bookmark").document(uid)
+        val updateAction = if (isBookmark) {
+            FieldValue.arrayUnion(articleId)
+        } else {
+            FieldValue.arrayRemove(articleId)
+        }
+
+        document.update("articleIds", updateAction).addOnFailureListener {
+            if (it is FirebaseFirestoreException && it.code == FirebaseFirestoreException.Code.NOT_FOUND) {
+                if (isBookmark) {
+                    document.set(hashMapOf("articleIds" to listOf(articleId)))
+                }
+            }
+        }
+    }
+
+    private fun isUserLoggedIn(): Boolean {
+        return Firebase.auth.currentUser != null
+    }
+
+    private fun navigateToWriteArticleFragment() {
+        val action = HomeFragmentDirections.actionHomeFragmentToWriteArticleFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun navigateToBookmarkArticleFragment() {
+        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToBookMarkArticleFragment())
+    }
+
+    private fun navigateToArticleFragment(articleId: String) {
+        findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToArticleFragment(articleId))
+    }
+
+    private fun showLoginSnackbar() {
+        Snackbar.make(
+            requireView(),
+            getString(R.string.guide_use_after_login),
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 }
